36adec4f2b01c9a0e73635afb9caba3ace65e871
HydraKane <hydralisk.nod@gmail.com>
2017-11-13 18:45:31 +0800

    Change All data to test SocketExpressExample

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
+// app.js
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-app.set('views', './views');
-app.set('view engine', 'jade');
-
-
-// Create a route handler for the home path
-app.get('/', function(req,res){
-	//res.send('Hello World');
-	res.render('index',{
-		title: 'My Page'
-	});
+app.use(express.static(__dirname + '/node_modules'));
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-var port = process.env.PORT || 3000;
 
-app.listen(port);
-console.log('Server listening at port ' + port);
+io.on('connection', function(client) {
+    console.log('Client connected...');
 
+    client.on('join', function(data) {
+	        console.log(data);
+	    });
 
+    client.on('messages', function(data) {
+		client.emit('broad', data);
+//		client.broadcast.emit('broad',data);
+           //client.broadcast.emit('broad',data);
+    });
+
+})
+
+server.listen(4200);
